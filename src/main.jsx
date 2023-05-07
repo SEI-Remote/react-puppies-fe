59906421218409b742b5932a5bd548be55a8e068
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2023-05-07 14:48:33 -0500

    build starter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
+// npm modules
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// app
 import App from './App.jsx'
+
+// css
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
