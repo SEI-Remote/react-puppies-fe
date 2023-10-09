af44141cbd19aeada46ac29614ec07001e860970
Ben Manley <benjamintmanley@gmail.com>
2023-10-09 08:56:09 -0500

    add react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 // npm modules
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 // app
 import App from './App.jsx'
@@ -10,6 +11,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 )
