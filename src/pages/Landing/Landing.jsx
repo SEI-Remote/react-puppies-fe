cf6546615fb9174eb1082fa1494952ba405923ca
Ben Manley <benjamintmanley@gmail.com>
2024-02-04 15:36:44 -0600

    update starter code

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,20 @@
+import PuppyLogo from '../../assets/animations/puppy-logo.riv'
+import { useRive } from '@rive-app/react-canvas'
+
+const Landing = () => {
+  // Rive
+  const { RiveComponent } = useRive({
+    src: PuppyLogo,
+    autoplay: true,
+    stateMachines: "State Machine 1",
+  })
+
+  return (
+    <main>
+      <RiveComponent />
+      <h1>REACT PUPPIES</h1>
+    </main>
+  )
+}
+
+export default Landing
