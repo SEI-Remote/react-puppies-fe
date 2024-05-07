c6885c95a979d327e4b115572c8a5bc008de4d8d
Ben Manley <benjamintmanley@gmail.com>
2024-05-07 08:37:32 -0500

    add Nav component and set up navigation and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+// npm modules
+import { Routes, Route, useNavigate } from 'react-router-dom'
 
+// pages
 import Landing from './pages/Landing/Landing'
+
+// components
+import Nav from './components/Nav/Nav'
+
+// styling
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Landing />
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/puppies' element={ <h1>PUPPY LIST</h1> } />
+      </Routes>
     </>
   )
 }
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+// npm modules
+import { NavLink } from "react-router-dom"
+
+// assets
+import logo from '../../assets/logo.svg'
+
+const Nav = () => {
+  return (
+    <nav>
+      <NavLink to='/'><img src={logo} alt="A cute puppy" /></NavLink>
+      <NavLink to='/puppies'>Puppy List</NavLink>
+    </nav>
+  )
+}
+
+export default Nav
