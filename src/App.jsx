ab63217d2f85bd2b43cbf75e7693ca739128afe8
ManliestBen <benjamintmanley@gmail.com>
2024-08-26 12:33:34 -0500

    add basic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+// npm modules
+import { Route, Routes, useNavigate } from 'react-router-dom'
 
+// pages
 import Landing from './pages/Landing/Landing'
+
+// components
+import Nav from './components/Nav/Nav'
+
+// css
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Landing />
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/puppies' element={<h1> PUPPY LIST </h1>} />
+      </Routes>
     </>
   )
 }
